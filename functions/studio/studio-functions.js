27385a70b3f43a3568d596b33ab82e36a10b157c
unknown <ryjwink@gmail.com>
2023-09-25 22:09:03 -0500

    changes to functions studio

--- functions/studio/studio-functions.js
+++ functions/studio/studio-functions.js
@@ -1,6 +1,11 @@
 //We want to COMPLETELY reverse an array by flipping the order of the entries AND flipping the order of characters in each element.
 
 // Part One: Reverse Characters
+function reverseCharacters(myVariableName){
+    return myVariableName.split('').reverse().join('');
+}
+myVariableName= 'apple';
+console.log(reverseCharacters(myVariableName));
 
 // 1. Define the function as reverseCharacters. Give it one parameter, which will be the string to reverse.
 // 2. Within the function, split the string into an array, then reverse the array.
@@ -10,6 +15,18 @@
 // 6. Optional: Use method chaining to reduce the lines of code within the function.
 
 // Part Two: Reverse Digits
+function reverseCharacters(myVariableName){
+   
+    if ( typeof myVariableName === 'string'){
+       myVariableName = myVariableName.split('').reverse().join('');
+}   else if ( typeof myVariableName === 'number'){
+    myVariableName = String(myVariableName).split('').reverse().join('');
+    myVariableName = Number(myVariableName);
+}
+    return myVariableName
+}
+myVariableName =8675309 ;
+console.log(reverseCharacters(myVariableName))
 
 // 1. Add an if statement to reverseCharacters to check the typeof the parameter.
 // 2. If typeof is ‘string’, return the reversed string as before.
@@ -30,19 +47,59 @@ let arrayTest1 = ['apple', 'potato', 'Capitalized Words'];
 let arrayTest2 = [123, 8897, 42, 1168, 8675309];
 let arrayTest3 = ['hello', 'world', 123, 'orange'];
 
+function completeReverse (oldArray){
+    array = [];
+    for ( let i = 0; i < oldArray.length; i++){
+        array[i] = reverseCharacters(oldArray[i])
+    }
+    return array
+}
+oldArray= arrayTest1
+console.log(completeReverse(oldArray));
+oldArray=arrayTest2
+console.log(completeReverse(oldArray));
+oldArray = arrayTest3
+console.log(completeReverse(oldArray));
+
 // Bonus Missions
 
 // 1. Have a clear, descriptive name like funPhrase.
 // 2. Retrieve only the last character from strings with lengths of 3 or less.
 // 3. Retrieve only the first 3 characters from strings with lengths larger than 3.
 // 4. Use a template literal to return the phrase We put the '___' in '___'. Fill the first blank with the modified string, and fill the second blank with the original string.
+function madLipps (phrase){
+    modifiedLine = '';
+    if (phrase.length <= 3){
+        modifiedLine = phrase.slice(2);
+    } else if (phrase.length > 3){
+        modifiedLine = phrase.slice(0,3);
+    }
+
+    return `We put the ${modifiedLine} in ${phrase}`;
+}
 
 // Test Function
+
+phrase = 'dylan';
+console.log(madLipps(phrase));
 
 // 1. Outside of the function, define the variable str and initialize it with a string (e.g. 'Functions rock!').
 // 2. Call your function and print the returned phrase.
 
 // Area of rectangle equal to length x width
+
+function areaOfRectangle( length, width){
+    area = '';
+    if (width === undefined){
+        area = length *length;
+    } else if (length === undefined){
+        area = width * width;
+    }else {
+        area = length * width;
+    }
+        return `The area is ${area} cm^2`
+}
+console.log(areaOfRectangle(4))
 
 // 1. Define a function with the required parameters to calculate the area of a rectangle.
 // 2. The function should return the area, NOT print it.
